fb5c395751b874b3f5d8ae5466f1248f7ac3b50e
bachtt <bachtt107@gmail.com>
2023-01-03 17:55:35 +0700

    project

--- food-blog/src/App.js
+++ food-blog/src/App.js
@@ -1,5 +1,80 @@
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
+import CategoryAddNew from "./module/category/CategoryAddNew";
+import CategoryManage from "./module/category/CategoryManage";
+import CategoryUpdate from "./module/category/CategoryUpdate";
+import DashboardLayout from "./module/dashboard/DashboardLayout";
+import PostAddNew from "./module/post/PostAddNew";
+import PostManage from "./module/post/PostManage";
+import UserAddNew from "./module/user/UserAddNew";
+import UserManage from "./module/user/UserManage";
+import UserProfile from "./module/user/UserProfile";
+import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import PostDetailsPage from "./pages/PostDetailsPage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+/**
+ *
+ * tat ca moi thu ben trong Routes nay co nhung component sau nay chung ta se render
+ * thi no co the truy xuat duoc gia tri ben trong authProvider
+ */
 function App() {
-  return <div className="App text-lg text-red-500 ">Welcome my Food blog</div>;
+  return (
+    <div>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
+          <Route path="/signin" element={<SignInPage></SignInPage>}></Route>
+          <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+          <Route
+            path="/:slug"
+            element={<PostDetailsPage></PostDetailsPage>}
+          ></Route>
+          <Route element={<DashboardLayout></DashboardLayout>}>
+            <Route
+              path="/dashboard"
+              element={<DashboardPage></DashboardPage>}
+            ></Route>
+            <Route
+              path="/manage/post"
+              element={<PostManage></PostManage>}
+            ></Route>
+            <Route
+              path="/manage/add-post"
+              element={<PostAddNew></PostAddNew>}
+            ></Route>
+            <Route
+              path="/manage/category"
+              element={<CategoryManage></CategoryManage>}
+            ></Route>
+            <Route
+              path="/manage/add-category"
+              element={<CategoryAddNew></CategoryAddNew>}
+            ></Route>
+            <Route
+              path="/manage/update-category"
+              element={<CategoryUpdate></CategoryUpdate>}
+            ></Route>
+            <Route
+              path="/manage/user"
+              element={<UserManage></UserManage>}
+            ></Route>
+            <Route
+              path="/manage/add-user"
+              element={<UserAddNew></UserAddNew>}
+            ></Route>
+            <Route
+              path="/profile"
+              element={<UserProfile></UserProfile>}
+            ></Route>
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App;
--- food-blog/src/index.js
+++ food-blog/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
 import "./styles/index.scss";
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { theme } from "./utils/constants";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles></GlobalStyles>
+      <BrowserRouter>
+        <App />
+        <ToastContainer></ToastContainer>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
-
 
 reportWebVitals();
